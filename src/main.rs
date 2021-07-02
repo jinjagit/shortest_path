d0e2ff0cae9933fc5b2b59d715a667799d924ab3
Simon Tharby <simon@liquidvoting.io>
2021-07-02 15:35:41 +0100

    Capitalize comments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod plot;
 
 fn main() {
-    // define coords to use for line series & points
+    // Define coords to use for line series & points
     let coords = vec![(0.3, 0.6), (0.7, 0.4), (0.8, 0.7), (0.1, 0.3)];
 
-    // pass coords to plot function
+    // Pass coords to plot function
     plot::plot(coords).unwrap();
 }
