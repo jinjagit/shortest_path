2532a6e84e6ed6465793954b03a2e74de10b2c04
Simon Tharby <simon@liquidvoting.io>
2021-07-02 15:34:03 +0100

    Remove example comments

--- src/plot.rs
+++ src/plot.rs
@@ -1,48 +1,31 @@
 use plotters::prelude::*;
 
-pub fn plot(
-
-    point_coords: std::vec::Vec<(f32, f32)>
-) -> Result<(), Box<dyn std::error::Error>> {
+pub fn plot(point_coords: std::vec::Vec<(f32, f32)>) -> Result<(), Box<dyn std::error::Error>> {
     let root = BitMapBackend::new("images/5.png", (1280, 1280)).into_drawing_area();
     root.fill(&WHITE).unwrap();
     let root = root.margin(10, 10, 10, 20);
-    // After this point, we should be able to draw construct a chart context
     let mut chart = ChartBuilder::on(&root)
-        // Set the caption of the chart
         .caption("Points & lines :)", ("sans-serif", 30).into_font())
-        // Set the size of the label region
         .x_label_area_size(20)
         .y_label_area_size(40)
-        // Finally attach a coordinate on the drawing area and make a chart context
         .build_cartesian_2d(0f32..1f32, 0f32..1f32)?;
 
-    // Then we can draw a mesh
-    chart
-        .configure_mesh()
-        // We can customize the maximum number of labels allowed for each axis
-        .x_labels(5)
-        .y_labels(5)
-        // We can also change the format of the label text
-        // .y_label_formatter(&|x| format!("{:.3}", x))
-        .draw()?;
+    // Draw mesh
+    chart.configure_mesh().x_labels(5).y_labels(5).draw()?;
 
-    // And we can draw something in the drawing area
+    // Draw line series
     let mut line_coords = point_coords.clone();
     line_coords.push(point_coords[0]);
 
     chart.draw_series(LineSeries::new(line_coords, &RED))?;
 
-    // Similarly, we can draw point series
+    // Draw point series
     chart.draw_series(PointSeries::of_element(
         point_coords,
         5, // Size of point
         &BLACK,
-        &|c, s, st| {
-            return EmptyElement::at(c)    // We want to construct a composed element on-the-fly
-            + Circle::new((0,0),s,st.filled()) // At this point, the new pixel coordinate is established
-            // + Text::new(format!("{:?}", c), (10, 0), ("sans-serif", 10).into_font());
-        },
+        &|c, s, st| return EmptyElement::at(c) + Circle::new((0, 0), s, st.filled()),
     ))?;
+
     Ok(())
 }
