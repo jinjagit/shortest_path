f76d098054791695e1c2c84e856b4b0a351f46a3
Simon Tharby <simon@liquidvoting.io>
2021-07-05 13:56:24 +0100

    Calculate Ant route distance in Ant implementation
    Change of mind. I cannot see any issue with placing this
    calculation in Ant implemenation.
    Also add vearious initatil variable declarations.

--- src/ants.rs
+++ src/ants.rs
@@ -16,14 +16,32 @@ impl Ant {
     fn has_visited(&self, city: usize) -> bool {
         return self.visited[city];
     }
+
+    fn route_length(&self, distance_matrix: Vec<Vec<f32>>) -> f32 {
+        let mut distance: f32 = distance_matrix[self.route[self.route.len() - 1]][self.route[0]];
+
+        for i in 0..self.route.len() - 1 {
+            distance += distance_matrix[self.route[i]][self.route[i + 1]];
+        }
+
+        distance
+    }
 }
 
 pub fn ant_force(coords: Vec<(f32, f32)>) {
     let n = coords.len();
     let mut ants: Vec<Ant> = vec![];
 
+    let c: f32 = 1.0; // The original number of trails, at the start of the simulation
+    let alpha: f32 = 1.0; // Controls the pheromone importance
+    let beta: f32 = 5.0; // Controls the distance priority. Should, generally, be > alpha.
+    let evaporation: f32 = 0.5; // The percent of pheromone evaporating every iteration
+    let Q: f32 = 500.0; // Info. about the total amount of pheromone left on the trail by each Ant
+    let n_ants: f32 = 0.8; // How many ants we'll use per city
+    let random_factor: f32 = 0.01;
+
     // Create matrix of distances between points.
-    // let distance_matrix: Vec<Vec<f32>> = utils::distance_matrix(coords.clone(), n);
+    let distance_matrix: Vec<Vec<f32>> = utils::distance_matrix(coords.clone(), n);
 
     let n_ants: usize = 3; // Set number of ants
 
